34d3334a644943c31c3aef809cf87286e5e4165f
Allan Zimmermann <allan@openrpa.dk>
2022-07-19 06:46:43 +0200

    add backup/restore script

---
+++ db-backup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+docker-compose exec mongodb rm /tmp/openflow.bak
+docker-compose exec mongodb /usr/bin/mongodump --db openflow --gzip --archive=/tmp/openflow.bak
+docker compose cp mongodb:/tmp/openflow.bak .
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+echo "Current Time : $current_time"
+cp openflow.bak openflow.$current_time.bak
---
+++ db-restore.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker compose cp openflow.bak mongodb:/tmp/
+docker-compose exec mongodb /usr/bin/mongorestore --gzip --archive=/tmp/openflow.bak
