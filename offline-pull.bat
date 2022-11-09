bb37cc5464587abd73ab8ca2c1c9f309dc6a3552
Allan Zimmermann <allan@openrpa.dk>
2022-11-09 14:00:12 +0100

    update ee license, add a few images to cache

--- offline-pull.bat
+++ offline-pull.bat
@@ -6,6 +6,10 @@ docker pull mongo
 docker image tag mongo:latest localhost:5000/mongo:latest
 docker image push localhost:5000/mongo:latest
 
+docker pull mongo-express
+docker image tag mongo:latest localhost:5000/mongo-express:latest
+docker image push localhost:5000/mongo-express:latest
+
 docker pull traefik
 docker image tag traefik:latest localhost:5000/traefik:latest
 docker image push localhost:5000/traefik:latest
@@ -21,6 +25,18 @@ docker image push localhost:5000/verdaccio/verdaccio
 docker pull iegomez/mosquitto-go-auth
 docker image tag iegomez/mosquitto-go-auth:latest localhost:5000/iegomez/mosquitto-go-auth
 docker image push localhost:5000/iegomez/mosquitto-go-auth
+
+docker pull otel/opentelemetry-collector
+docker image tag otel/opentelemetry-collector:latest localhost:5000/otel/opentelemetry-collector:latest
+docker image push localhost:5000/otel/opentelemetry-collector:latest
+
+docker pull victoriametrics/victoria-metrics
+docker image tag victoriametrics/victoria-metrics:latest localhost:5000/victoriametrics/victoria-metrics:latest
+docker image push localhost:5000/victoriametrics/victoria-metrics:latest
+
+docker pull openiap/grafana
+docker image tag openiap/grafana:latest localhost:5000/openiap/grafana:latest
+docker image push localhost:5000/openiap/grafana:latest
 
 
 
