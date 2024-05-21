a0a1b7427fb8fbba91f8103bf4a12143de6e19ab
skadefro <allan@openrpa.dk>
2024-05-21 09:30:38 +0200

    pre-download agent and noderedagent

--- multiple-up.sh
+++ multiple-up.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 docker compose -f docker-compose-multiple.yml -p demo up -d
+docker pull openiap/noderedagent:latest
+docker pull openiap/nodeagent:latest
--- offline-up.bat
+++ offline-up.bat
@@ -1,2 +1,4 @@
 @echo off
 docker compose -f docker-compose-offline.yml -p demo up -d
+docker pull openiap/noderedagent:latest
+docker pull openiap/nodeagent:latest
