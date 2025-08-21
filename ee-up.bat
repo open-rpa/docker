f9084025da22cc1d29745a29c6efba8890540b46
skadefro <allan@openrpa.dk>
2025-08-21 08:16:58 +0200

    Add instructions

--- ee-up.bat
+++ ee-up.bat
@@ -1,2 +1,3 @@
 @echo off
 docker compose -f docker-compose-ee.yml -p demo up -d
+@echo "If this is the first time you run this, please run the command again, to finish setting up mongodb replica set."
--- letsencrypt-up.bat
+++ letsencrypt-up.bat
@@ -2,3 +2,4 @@
 docker compose -f docker-compose-letsencrypt.yml -p demo up -d
 docker pull openiap/noderedagent:latest
 docker pull openiap/nodeagent:latest
+@echo "If this is the first time you run this, please run the command again, to finish setting up mongodb replica set."
